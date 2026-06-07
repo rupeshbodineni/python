ad5e9da386311db8574341fdd1d1fb7c7f73522f
rupeshbodineni <rupeshbodineni@gmail.com>
2026-06-07 22:34:00 +0530

    R

---
+++ day14/traverse.py
@@ -0,0 +1,13 @@
+#Traversal
+arr=[10,20,30,40,50]
+for i in arr:
+    print(i)
+
+#list methods
+arr=[10,20,30,40,50]
+print("original list:",arr)
+
+#append
+arr=[10,20,30,40,50]
+arr.append(60)
+print(arr)
