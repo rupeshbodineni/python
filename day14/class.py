f3b1cd1347a0e057e5fe260dc28e5ea1cdf908aa
rupeshbodineni <rupeshbodineni@gmail.com>
2026-06-09 12:33:38 +0530

    R

--- day14/class.py
+++ day14/class.py
@@ -4,12 +4,22 @@
 
 
 #create 
-a=15       #Decimal Format
-b=0b1111   #Binary Format
-c=0o17     #Octal Format
-d=0xF      #Hexa Decimal Format
+# a=15       #Decimal Format
+# b=0b1111   #Binary Format
+# c=0o17     #Octal Format
+# d=0xF      #Hexa Decimal Format
 #read
-print(a)
-print(b)
-print(c)
-print(d)
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+enames = ["rahul", "rupesh", "suresh", "ramesh"]
+
+print(enames[0])
+print(enames[3])
+print(enames[2])
+
+enames[0] = "rupesh"
+
+print(enames)
