ee46b150de59e773bb463d6306d9f497d0f062e3
rupeshbodineni <rupeshbodineni@gmail.com>
2026-06-09 12:12:06 +0530

    R

--- day14/class.py
+++ day14/class.py
@@ -1,3 +1,15 @@
-print(bin(15))
-print(oct(15))
-print(hex(15))
+# print(bin(15))
+# print(oct(15))
+# print(hex(15))
+
+
+#create 
+a=15       #Decimal Format
+b=0b1111   #Binary Format
+c=0o17     #Octal Format
+d=0xF      #Hexa Decimal Format
+#read
+print(a)
+print(b)
+print(c)
+print(d)
