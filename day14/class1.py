39ba02d1a2d2b3c9717bf6548f972b94085e7f45
rupeshbodineni <rupeshbodineni@gmail.com>
2026-06-09 12:45:41 +0530

    R

---
+++ day14/class1.py
@@ -0,0 +1,13 @@
+# a=10,20
+# print(type(a))
+# b=()
+# print(type(b))
+
+enames = ["rahul", "rupesh", "suresh", "ramesh"]
+unames = ("rahul", "rupesh", "suresh", "ramesh")
+
+enames[0]="virat"
+print(enames)
+
+# unames[0]="virat"
+# print(unames)
