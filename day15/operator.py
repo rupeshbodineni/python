f1d306f043c9410142ca3865ea17b8e2f8bdaf9b
rupeshbodineni <rupeshbodineni@gmail.com>
2026-06-10 12:23:04 +0530

    R

---
+++ day15/operator.py
@@ -0,0 +1,6 @@
+a=10
+b=20
+c=30
+d="rahul"
+print(a+b)
+print(c+d)
--- day15/set.py
+++ day15/set.py
@@ -1,3 +1,32 @@
-a=set()
-print(a)
-print (type(a))
+# a=set()
+# print(a)
+# print (type(a))
+
+# b={10,10,10,10,10}
+# print(b)
+
+# b=20
+# print(b)
+
+# print(b[2])
+
+# emp={
+#     'ename':'rahul',
+#     'eid':101,
+#     'esal':4500.45
+# }
+
+# print(emp['eid'])
+# print(emp['ename'])
+# print(emp['location'])
+
+user={
+    'uid':102,
+    'uname':'rupesh',
+    'location':'banglore'
+}
+
+
+print(user["uid"])
+print(user["uname"])
+print(user['location'])
