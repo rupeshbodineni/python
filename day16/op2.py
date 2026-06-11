3bc303d4d37163b4ea2ff974e621f4d0a4a994ff
rupeshbodineni <rupeshbodineni@gmail.com>
2026-06-11 12:28:41 +0530

    R

---
+++ day16/op2.py
@@ -0,0 +1,5 @@
+num=int(input("Enter a number:"))
+if num>99 and num<=999:
+    print("it is a 3 digit number")
+else:
+    print("not a 3 digit number")
---
+++ day16/op3.py
@@ -0,0 +1,9 @@
+# num=int(input("enter a number:"))
+# if num%2==0:
+#     print("it is divisible by 2")
+# else:
+#     print("it is not divisible by 2")
+
+eids=[10,20,30,40]
+for eid in eids:
+    print("good morning")
