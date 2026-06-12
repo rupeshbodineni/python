55cf2a48272a48631bc6852cc37472e84d78c5ab
rupeshbodineni <rupeshbodineni@gmail.com>
2026-06-12 21:19:09 +0530

    R

---
+++ day17/division.py
@@ -0,0 +1,7 @@
+num1=float(input("enter the dividend for division:"))
+num2=float(input("enter the divisior for division:"))
+if num1==0:
+    print("division by zero is not allowed")
+else:
+    result=num1/num2
+    print(f"{num1}/{num2}={result}")
