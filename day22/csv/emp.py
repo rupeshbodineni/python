1fe5740189dba32e4a6881467447cd2fdcf672c1
rupeshbodineni <rupeshbodineni@gmail.com>
2026-06-19 12:50:25 +0530

    R

--- day22/csv/emp.py
+++ day22/csv/emp.py
@@ -4,3 +4,11 @@ fp1=open('user.csv','r')
 csv_reader=csv.reader(fp1)
 users=list(csv_reader)
 print(len(users))
+emp_data=[]
+for emp in emp_data:
+    emp_data.append({"id":users[0],
+                     "name":users[1],
+                     "gender":users[2]
+                          })
+fp2=open('emp.json','w')
+json.dump
