d47cd425c197f76403e9f59541dddf3d2c2a260f
rupeshbodineni <rupeshbodineni@gmail.com>
2026-06-20 12:18:01 +0530

    R

--- day2/crud.py
+++
@@ -1,6 +0,0 @@
-price=400
-rating=4.5
-print(price)
-print(rating)
-print(type(price))
-print(type(rating))
---
+++ day22/csv/graph.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+x = [100, 200, 300, 400]
+y = [20, 40, 60, 60]
+
+plt.plot(x, y)
+
+plt.xlabel("sales")
+plt.ylabel("products")
+plt.title("Simple Line Chart")
+
+plt.show()
