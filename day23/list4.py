77adbd9c097378d65e1bb9a926039ab0df18af63
rupeshbodineni <rupeshbodineni@gmail.com>
2026-06-23 12:14:05 +0530

    R

--- day23/list4.py
+++ day23/list4.py
@@ -1,2 +1,17 @@
 nums={10,20,30,40,50}
 print(list(nums))
+
+words = ['apple', 'banana', 'cherry']
+firstletter=[word[0] for word in words]
+print(firstletter)
+
+matrix=[[j for j in range(3)] for i in range(3)]
+print(matrix)
+
+a,b,c=10,20,30
+print(a)
+
+nums=[10,20,30,40]
+a,b,c,d=nums
+print(c)
+print(d)
