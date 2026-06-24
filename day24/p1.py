b2ba7342316f1365f88edf299e10a1ed21586c46
rupeshbodineni <rupeshbodineni@gmail.com>
2026-06-24 13:27:47 +0530

    R

--- day24/p1.py
+++ day24/p1.py
@@ -1,9 +1,5 @@
-#Extract data from Rest API
-import requests,json
-resp=requests.get('https://jsonplaceholder.typicode.com/users')
-users=resp.json()
-print(len(users))
-
+print(len(users))  #10
+#Transform data- for json file and csv file
 users_json=[]
 for user in users:
     users_json.append({
@@ -14,7 +10,7 @@ for user in users:
     })
 print(len(users_json))
 print(users_json)
+#Load data into new json file and csv file
 fp1=open('user.json','w')
 json.dump(users_json,fp1)
-fp1.close()
 print("New Data file created")
