70143ddbb97b1776acbc34c03386b2017e5a4f35
rupeshbodineni <rupeshbodineni@gmail.com>
2026-06-24 10:21:32 +0530

    R

--- bank management/bank.py
+++ bank management/bank.py
@@ -20,9 +20,29 @@ class bankmanagement():
         print(f"account holder:{self.name}")    
         print(f"account balance:{self.balance}") 
 
-user=bankmanagement("rupesh",5000)
-user.deposit(2000)
-user.check_balance()
-user.withdraw(1000)
+name=input("enter yout name:")
+balance=float(input("enter opening bonus:"))
+account=bankmanagement(name,balance)
+while True:
+    print("1.deposit")
+    print("2.withdraw")
+    print("3.checkbalance")
+    print("4.exit")
 
-user.check_balance()
+    choice=int(input("select an option:"))
+    if choice==1:
+        amount=float(input("enter the amount:"))
+        account.deposit(amount)
+
+    elif choice==2:
+        amount=int(input("enter the withdrawn amount:"))
+        account.withdraw(amount)
+
+    elif choice==3:
+        account.check_balance()
+
+    elif choice==4:
+        print("thank you for using bank management system")
+        break
+    else:
+        print("invalid choice")
