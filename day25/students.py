567a8efffa641ebdd17e2c88b718076b750901c6
rupeshbodineni <rupeshbodineni@gmail.com>
2026-06-29 18:43:57 +0530

    R

---
+++ day25/students.py
@@ -0,0 +1,18 @@
+import json
+f1=open("students.json",'r')
+students=json.load(f1)
+
+# for student in students:
+#     print(student['name'],student['course'])
+
+
+# for student in students:
+#     print(student['name'])
+
+# for student in students:
+#     if student['cgpa']>9:
+#         print(student['name'],student['cgpa'])
+
+
+for student in students:
+    where 
