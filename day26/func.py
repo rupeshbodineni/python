5c6f727b9b0ca572b064ad539353fb0e00398248
rupeshbodineni <rupeshbodineni@gmail.com>
2026-07-04 21:29:57 +0530

    R

---
+++ day26/func.py
@@ -0,0 +1,16 @@
+def index(name,status):
+    print("home page")
+
+def product_page(name,status):
+    print("product page")
+
+def order_page(name,status):
+    print("order page")
+
+def profile(name,status):
+    print("profile page")
+
+index("rahul",True)
+product_page("rupesh",False)
+order_page("prabhas",True)
+profile("suresh",True)
