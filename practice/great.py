8c53595d9566325ef90493f3f0e26dbc38b23f1e
rupeshbodineni <rupeshbodineni@gmail.com>
2026-07-06 12:03:05 +0530

    R

--- practice/great.py
+++ practice/great.py
@@ -21,6 +21,21 @@
 # print(bool(""))
 
 
-print(bool([]))
-print(bool([10]))
+# print(bool([]))
+# print(bool([10]))
 
+# a="4"
+# b=4
+# print(a==b)
+
+# x=[1,2,3]
+# y=x
+# y.append(4)
+# print(x)
+# print(y)
+
+sum=0
+for i in range(1,11):
+    if i%2==0 and i%3==0:
+        sum+=0
+print(sum)
