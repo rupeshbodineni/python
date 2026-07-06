a8324f5df5eef5c822762bfaec2b349ef2b15ffb
rupeshbodineni <rupeshbodineni@gmail.com>
2026-07-06 11:55:12 +0530

    R

--- practice/great.py
+++ practice/great.py
@@ -1,11 +1,26 @@
-n=5
-n=n>3
-print(n+10)
+# n=5
+# n=n>3
+# print(n+10)
 
 
-x = 10
-x = x < 5
-print(x+10)
+# x = 10
+# x = x < 5
+# print(x+10)
 
-print(True==1)
-print(False==0)
+# print(True==1)
+# print(False==0)
+
+
+# print(True is 1)
+
+# print(int(True))
+
+# print(bool(0))
+# print(bool(10))
+
+# print(bool(""))
+
+
+print(bool([]))
+print(bool([10]))
+
