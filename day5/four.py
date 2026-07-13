94d679306aba088b57a725bf85ceb40e4c1854b7
rupeshbodineni <rupeshbodineni@gmail.com>
2026-07-13 21:36:42 +0530

    R

--- day5/four.py
+++ day5/four.py
@@ -1,3 +1,17 @@
+emp={
+    'eid':101,
+    'ename':"Rahul",
+    'esal':45000,
+}
+print(emp['eid'])
+print(emp['ename'])
+print(emp['esal'])
+print(emp['loc'])
+
+print(emp)
+print(emp['ename'])
+print(emp['esal'])
+print(emp['loc'])
 
 #                    list                 tuple                   set
 #duplicates           yes                  yes                     no
