15c5fdedf41bea1faa4cc629220b8a57dc9e4a66
rupeshbodineni <rupeshbodineni@gmail.com>
2026-07-17 22:09:19 +0530

    traverse

---
+++ day5/three.py
@@ -0,0 +1,26 @@
+numbers={10,"Rahul",True}
+print(numbers)
+s1=[10,20,30,40]
+s2=(10,20,30,40)
+s3={10,20,30,40,50}
+s4={
+    "eid":101,
+    "ename":"Rahul"
+}
+
+print(s1)
+print(s2)
+print(s3)
+print(s4)
+
+s1.clear()
+print(s1)
+
+s2.clear()
+print(s2)
+
+s3.clear()
+print(s3)
+
+s4.clear()
+print(s4)
